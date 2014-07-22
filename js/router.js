63b2184e574f117be4426eea3f8476afe068b025
wexoni <vedranma@maestralsolutions.com>
2014-07-22 17:06:07 +0200

    Small step for me. Giant leap for knowledge

---
+++ js/router.js
@@ -0,0 +1,12 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: VedranMa
+ * Date: 7/22/14
+ * Time: 5:01 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+
+Todos.Router.map(function() {
+    this.resource('todos', { path: '/' });
+});
